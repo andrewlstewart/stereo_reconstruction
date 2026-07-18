dccd849eee72e02a49ba8a97b886f42e6437688a
andrewlstewart <andrewlstewart@gmail.com>
2026-07-18 12:04:42 -0400

    Update to set center to int type

--- src/stereo_reconstruction/utils.py
+++ src/stereo_reconstruction/utils.py
@@ -19,8 +19,8 @@ def drawlines(img1, img2, lines, pts1, pts2):
         x0, y0 = map(int, [0, -r[2] / r[1]])
         x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1]])
         img1 = cv2.line(img1, (x0, y0), (x1, y1), color, 1)
-        img1 = cv2.circle(img1, tuple(pt1), 5, color, -1)
-        img2 = cv2.circle(img2, tuple(pt2), 5, color, -1)
+        img1 = cv2.circle(img1, center=tuple(pt1.astype(np.int64)), radius=5, color=color, thickness=-1)
+        img2 = cv2.circle(img2, tuple(pt2.astype(np.int64)), 5, color, -1)
     return img1, img2
 
 
